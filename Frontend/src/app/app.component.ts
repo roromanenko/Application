29f57eaaa5c39178d9f95f692451e18d8efc6e85
Roman Romanenko <roman.roromanenko@proton.me>
2025-11-02 09:29:16 +0200

    Update Home page. Add navbar

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Event Management System';
+  title = 'Organiza';
 }
